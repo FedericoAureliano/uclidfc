8f9c54b79169fff7fd1e9ad483eb03745f6a1228
FedericoAureliano <fmorarocha@gmail.com>
2021-02-15 23:52:45 -0800

    if you don't type check, then you technically support all types...(smtlib theories)

--- src/main/scala/com/uclid/solverinterface/compiler/SmtCompiler.scala
+++ src/main/scala/com/uclid/solverinterface/compiler/SmtCompiler.scala
@@ -41,59 +41,7 @@ object SmtCompiler {
     val local: HashMap[String, Int] = new HashMap()
     var termgraph = new TermGraph()
     var ctx = new SyMTContext(termgraph)
-
-    val ite = ctx.termgraph.memoAddInstruction(TheoryMacro("ite"))
-    val plus = ctx.termgraph.memoAddInstruction(TheoryMacro("+"))
-    val minus = ctx.termgraph.memoAddInstruction(TheoryMacro("-"))
-    val times = ctx.termgraph.memoAddInstruction(TheoryMacro("*"))
-    val concat = ctx.termgraph.memoAddInstruction(TheoryMacro("str.++"))
-    val indexof = ctx.termgraph.memoAddInstruction(TheoryMacro("str.indexof"))
-    val substr = ctx.termgraph.memoAddInstruction(TheoryMacro("str.substr"))
-    val len = ctx.termgraph.memoAddInstruction(TheoryMacro("str.len"))
-    val contains = ctx.termgraph.memoAddInstruction(TheoryMacro("str.contains"))
-    val prefixof = ctx.termgraph.memoAddInstruction(TheoryMacro("str.prefixof"))
-    val suffixof = ctx.termgraph.memoAddInstruction(TheoryMacro("str.suffixof"))
-    val replace = ctx.termgraph.memoAddInstruction(TheoryMacro("str.replace"))
-    val at = ctx.termgraph.memoAddInstruction(TheoryMacro("str.at"))
-    val gt = ctx.termgraph.memoAddInstruction(TheoryMacro(">"))
-    val lt = ctx.termgraph.memoAddInstruction(TheoryMacro("<"))
-    val gte = ctx.termgraph.memoAddInstruction(TheoryMacro(">="))
-    val lte = ctx.termgraph.memoAddInstruction(TheoryMacro("<="))
-    val eq = ctx.termgraph.memoAddInstruction(TheoryMacro("="))
-    val and = ctx.termgraph.memoAddInstruction(TheoryMacro("and"))
-    val or = ctx.termgraph.memoAddInstruction(TheoryMacro("or"))
-    val not = ctx.termgraph.memoAddInstruction(TheoryMacro("not"))
-    val falseT = ctx.termgraph.memoAddInstruction(TheoryMacro("false"))
-    val trueT = ctx.termgraph.memoAddInstruction(TheoryMacro("true"))
-
-
-    val interpretedSymbols = Map(
-      ("ite", ite),
-      ("+", plus),
-      ("-", minus),
-      ("*", times),
-      ("str.++", concat),
-      ("str.indexof", indexof),
-      ("str.substr", substr),
-      ("str.len", len),
-      ("str.contains", contains),
-      ("str.prefixof", prefixof),
-      ("str.suffixof", suffixof),
-      ("str.replace", replace),
-      ("str.at", at),
-      (">", gt),
-      ("<", lt),
-      (">=", gte),
-      ("<=", lte),
-      ("=", eq),
-      ("and", and),
-      ("or", or),
-      ("not", not),
-      ("false", falseT),
-      ("true", trueT)  
-    )
-
-
+    
     var pos = 0
     var nest = 0
 
@@ -229,14 +177,12 @@ object SmtCompiler {
       } else if (global.containsKey(tokens(pos - 1))) {
         global.get(tokens(pos - 1))
       } else {
-        interpretedSymbols.getOrElse(
-          tokens(pos - 1),
-          tokens(pos - 1) match {
-            case other if other.startsWith("\"") && other.endsWith("\"") =>
-              ctx.termgraph.memoAddInstruction(TheoryMacro(other))
-            case other =>
-              ctx.termgraph.memoAddInstruction(TheoryMacro(other))
-          }) 
+        tokens(pos - 1) match {
+          case other if other.startsWith("\"") && other.endsWith("\"") =>
+            ctx.termgraph.memoAddInstruction(TheoryMacro(other))
+          case other =>
+            ctx.termgraph.memoAddInstruction(TheoryMacro(other))
+        }
       }
     ctx
   }
